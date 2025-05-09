075455441fdffeb499c110e42cbf24f2869eb0a7
lakshya <lakshyamogha1@gmail.com>
2025-05-09 06:57:47 +0530

    init

---
+++ data/page.js
@@ -0,0 +1,106 @@
+const PAGES_LINKING = {
+  0: [
+    {
+      x: 90,
+      y: 4,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/true-false/chap_1_ws_1_3.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+  ],
+  2: [
+    {
+      x: 90,
+      y: 4,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/mcq_new/chap_1_ws_2_3.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+    {
+      x: 92,
+      y: 38,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/mcq_new/chap_1_ws_2_4.html",
+      // link: "resources/interactivities/odd-one-out/index.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+  ],
+  3: [
+    {
+      x: 90,
+      y: 20,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/odd-one-out/chap_1_ws_3_1.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+  ],
+  4: [
+    {
+      x: 90,
+      y: 42,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/match-columns/chap_1_ws_3_5.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+  ],
+  5: [
+    {
+      x: 90,
+      y: 68,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/fib/chap_2_ws_4_2.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+  ],
+  7: [
+    {
+      x: 90,
+      y: 20,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/fib/chap_2_ws_5_1.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+    {
+      x: 90,
+      y: 74,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/mcq_new/chap_2_ws_5_2.html",
+      type: "iframe",
+      size: "1024x700",
+    },
+  ],
+  8:[
+    {
+      x: 90,
+      y: 42,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/match-columns/chap_2_ws_5_3.html",
+      type: "iframe",
+      size: "1024x700",
+    },{
+      x: 90,
+      y: 62,
+      title: "Interactivity",
+      icon: "img/icons/interactivites.gif",
+      link: "resources/interactivities/mcq_new/chap_2_ws_5_4.html",
+      type: "iframe",
+      size: "1024x700",
+    }
+  ]
+};
